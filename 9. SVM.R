b438f2313e8d31395a0ee55bf6eb8f74a166b97b
Unknown <dia151378@uvg.edu.gt>
2019-04-21 16:39:19 -0600

    primer avance

--- 9. SVM.R
+++ 9. SVM.R
@@ -1,23 +1,25 @@
-#Support Vector Machine
-
 library(e1071)
-library(caret)
-porcentaje<-0.7
-datos<-iris
-set.seed(123)
 
+library(caret)
+
+porcentaje<-0.7
+
+datos<-perrosCompleto
+set.seed(123)
 
 corte <- sample(nrow(datos),nrow(datos)*porcentaje)
 train<-datos[corte,]
 test<-datos[-corte,]
 
-plot(train$Sepal.Length,train$Petal.Length)
-modeloLineal<-lm(train$Sepal.Length~train$Petal.Length)
+plot(train$Age,train$Breed1)
+modeloLineal<-lm(train$Age~train$Breed1)
 abline(modeloLineal)
 
-modeloSVM<-svm(Species~., data=train, cost=2^5, gamma=2^-5)
+
+modeloSVM<-svm(RescuerID~., data=train, cost=2^5, gamma=2^-5)
 prediccion<-predict(modeloSVM,newdata=test)
 
-confusionMatrix(test$Species,prediccion)
+
+confusionMatrix(test$RescuerID,prediccion)
 
 
