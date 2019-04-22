a9d2bcdcbdc4ff2b681eaef486cf9aee367425a5
Ana lucia Diaz <LEPPEDIAZ@users.noreply.github.com>
2019-04-22 14:19:19 -0600

    Update 9. SVM.R

--- 9. SVM.R
+++ 9. SVM.R
@@ -1,11 +1,10 @@
 library(e1071)
-
 library(caret)
 
 porcentaje<-0.7
 
 datos<-perrosCompleto
-set.seed(123)
+set.seed(1234)
 
 corte <- sample(nrow(datos),nrow(datos)*porcentaje)
 train<-datos[corte,]
@@ -15,11 +14,12 @@ plot(train$Age,train$Breed1)
 modeloLineal<-lm(train$Age~train$Breed1)
 abline(modeloLineal)
 
+datos$AdoptionSpeed <- as.factor(datos$AdoptionSpeed)
 
-modeloSVM<-svm(RescuerID~., data=train, cost=2^5, gamma=2^-5)
+
+modeloSVM<-svm(AdoptionSpeed~., data=train, cost=2^1, gamma=2^-1, type = 'C')
 prediccion<-predict(modeloSVM,newdata=test)
 
 
-confusionMatrix(test$RescuerID,prediccion)
-
+confusionMatrix(test$AdoptionSpeed,prediccion)
 
