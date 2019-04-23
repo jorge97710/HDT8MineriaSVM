2de993e2635f7aa0cd29d38b4474c99685ae1430
Unknown <dia151378@uvg.edu.gt>
2019-04-22 18:02:15 -0600

    load data

--- 9. SVM.R
+++ 9. SVM.R
@@ -23,4 +23,25 @@ as.factor(modeloSVM)
 as.numeric(modeloSVM)
 prediccion<-predict(modeloSVM,newdata=test)
 confusionMatrix(test$AdoptionSpeed,prediccion)
+#segundo dato
+
+porcentaje<-0.7
+datos<-perrosCompleto
+set.seed(1234)
+
+corte <- sample(nrow(datos),nrow(datos)*porcentaje)
+train<-datos[corte,]
+test<-datos[-corte,]
+
+
+plot(train$Breed2,train$Breed1)
+modeloLineal<-lm(train$Breed2~train$Breed1)
+abline(modeloLineal)
+
+datos$AdoptionSpeed <- as.factor(datos$AdoptionSpeed)
+modeloSVM<-svm(AdoptionSpeed~., data=train, cost=2^1, gamma=2^-1, type = 'C')
+prediccion<-predict(modeloSVM,newdata=test)
+
+confusionMatrix(test$AdoptionSpeed,prediccion)
+
 
